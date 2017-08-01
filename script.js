45ef1c11ad5c8ef279ba6e5c81d2143b9050df87
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-01 15:34:15 -0400

    change API call to HTTPS to work with github pages

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("#quote").on('click',function(){
     //API call to get text as JSON object
-    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(returned){
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(returned){
       //console.log(returned);
       var quotetext = returned['quoteText'];
       var quoteauthor = "-" + returned['quoteAuthor'];
