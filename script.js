94128c6a737a573716d9e0246686d352f04a84d9
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-01 16:14:10 -0400

    better API get call using CORS anywhere

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
   $("#quote").on('click',function(){
     //API call to get text as JSON object
-    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(returned){
-      //console.log(returned);
+    $.get("https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&jsonp=?", function(returned){
+      console.log(returned);
+      //returned = $.parseJSON(returned);
       var quotetext = returned['quoteText'];
       var quoteauthor = "-" + returned['quoteAuthor'];
-      //console.log(quotetext + "  " + quoteauthor);
-      //console.log(typeof quotetext);
+      console.log(quotetext + "  " + quoteauthor);
+      console.log(typeof quotetext);
       var tweettext = quotetext.split("")
       var tweetlong = tweettext.splice(120);
       tweettext[-1] = "...";
