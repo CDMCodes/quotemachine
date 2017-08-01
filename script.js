feee5e6e2c6f8fb42a306cff7230c58f23642f0c
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-01 13:23:27 -0400

    successful API call and modification of html

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-
   $("#quote").on('click',function(){
+    //API call to get text as JSON object
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(returned){
-      console.log(returned);
+      //console.log(returned);
+      var quotetext = returned['quoteText'];
+      var quoteauthor = returned['quoteAuthor'];
+      //console.log(quotetext + "  " + quoteauthor);
+      //console.log(typeof quotetext);
+      $("#quotebox").html("<p>"+quotetext+"</p> <p>" + quoteauthor + "</p>");
     });
   });
 
-  // $("#quote").on('click',function(){
-  //   $.getJSON("https://thesimpsonsquoteapi.glitch.me/quotes", function(data){
-  //     console.log(data);
-  //   });
-  // });
 
 });
