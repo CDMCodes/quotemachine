cc6fa700ea278c0c3563d101273c7413ee52a693
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-01 07:51:45 -0400

    successful call to api copied from help forums

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
+  $("#quote").on('click',function(){
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(returned){
+      console.log(returned);
+    });
+  });
+
   // $("#quote").on('click',function(){
-  //   $.getJSON("http://api.forismatic.com/api/1.0/", function(data){
+  //   $.getJSON("https://thesimpsonsquoteapi.glitch.me/quotes", function(data){
   //     console.log(data);
   //   });
   // });
 
-  $("#quote").on('click',function(){
-    $.getJSON("https://thesimpsonsquoteapi.glitch.me/quotes", function(data){
-      console.log(data);
-    });
-  });
-  
 });
