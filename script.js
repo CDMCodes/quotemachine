636eb0b6b26960e894e94f3fa51d776f1cac358e
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-01 15:28:50 -0400

    complete css styling

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(returned){
       //console.log(returned);
       var quotetext = returned['quoteText'];
-      var quoteauthor = returned['quoteAuthor'];
+      var quoteauthor = "-" + returned['quoteAuthor'];
       //console.log(quotetext + "  " + quoteauthor);
       //console.log(typeof quotetext);
       var tweettext = quotetext.split("")
