4f33c1a67926b599f010ed7d9a8260377447a1e7
Andrew Campbell <campbell.drew.m@gmail.com>
2017-08-02 21:41:05 -0400

    moved API call to name function and call it on doc ready

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-  $("#quote").on('click',function(){
-    //API call to get text as JSON object
+  function getQuote(){
     $.get("https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&jsonp=?", function(returned){
-      console.log(returned);
+      //console.log(returned);
       //returned = $.parseJSON(returned);
       var quotetext = returned['quoteText'];
       var quoteauthor = "-" + returned['quoteAuthor'];
-      console.log(quotetext + "  " + quoteauthor);
-      console.log(typeof quotetext);
+      //console.log(quotetext + "  " + quoteauthor);
+      //console.log(typeof quotetext);
       var tweettext = quotetext.split("")
       var tweetlong = tweettext.splice(120);
       tweettext[-1] = "...";
@@ -15,7 +14,10 @@ $(document).ready(function(){
       $("#quotebox").html("<p>"+quotetext+"</p> <p>" + quoteauthor + "</p>");
       $("#tweetButton").parent().attr("href","https://twitter.com/intent/tweet?text=" + tweettext)
     });
-  });
+  }
 
+  getQuote();
+
+  $("#quote").on('click',getQuote);
 
 });
